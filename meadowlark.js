96517f830a2c292071faf6901950febc641a62fa
Prateek Jadhwani <prateekjadhwani@users.noreply.github.com>
2015-05-22 13:16:29 -0400

    Exercise on Page 24-26
    Views and Layouts
    - Adding `express3-handlebars` package
    - using handlebars in the project

--- meadowlark.js
+++ meadowlark.js
@@ -2,30 +2,34 @@ var express = require("express");
 
 var app = express();
 
+// setup handlebars view engine
+var handlebars = require('express3-handlebars')
+					.create({ 
+						'defaultLayout' : 'main'
+					});
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars')
+
 app.set("port", process.env.PORT || 3000);
 
 app.get('/', function(req, res) {
-	res.type("text/plain");
-	res.send("Meadowlark Travel");
+	res.render('home');
 });
 app.get('/about', function(req, res) {
-	res.type('text/plain');
-	res.send("About Meadowlark Travel");
+	res.render('about');
 });
 
 // custom 404
 app.use( function(req, res) {
-	res.type("text/plain");
 	res.status(404);
-	res.send('404 - Not Found');
+	res.render('404');
 });
 
 // custom 500
 app.use( function(err, req, res, next) {
 	console.log(err.stack);
-	res.type("text/plain");
 	res.status(500);
-	res.send('500 - Server Error');
+	res.render('500');
 });
 
 app.listen( app.get('port'), function() {

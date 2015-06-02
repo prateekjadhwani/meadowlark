c993408932b0f6f1bcfa8037d817ed1caab3d53c
Prateek Jadhwani <prateekjadhwani@users.noreply.github.com>
2015-06-01 20:56:32 -0400

    Exercise on Page 77-79
    Partials

--- meadowlark.js
+++ meadowlark.js
@@ -16,6 +16,12 @@ app.set("port", process.env.PORT || 3000);
 app.use(express.static(__dirname + '/public'));
 
 app.use( function(req, res, next) {
+	if(!res.locals.partials) res.locals.partials = {};
+	res.locals.partials.weather = getWeatherData();
+	next();
+});
+
+app.use( function(req, res, next) {
 	res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
 	next();
 });
@@ -38,6 +44,7 @@ app.get('/tours/hood-river', function(req, res) {
 	res.render('tours/hood-river');
 });
 
+
 // custom 404
 app.use( function(req, res) {
 	res.status(404);
@@ -55,3 +62,32 @@ app.listen( app.get('port'), function() {
 	console.log( 'Express started on http://localhost:' + 
 			   app.get('port') + '; press Ctrl+C to terminate.');
 });
+
+
+function getWeatherData() {
+	return {
+		locations : [
+			{
+				name: 'Portland',
+				forecastUrl: 'http://www.wunderground.com/US/OR/Portland.html',
+				iconUrl: 'http://icons-ak.wxug.com/i/c/k/cloudy.gif',
+				weather: 'Overcast',
+				temp: '54.1 F (12.3 C)'
+			},
+			{
+				name: 'Bend',
+				forecastUrl: 'http://www.wunderground.com/US/OR/Bend.html',
+				iconUrl: 'http://icons-ak.wxug.com/i/c/k/partlycloudy.gif',
+				weather: 'Partly Cloudy',
+				temp: '55.0 F (12.8 C)'
+			},
+			{
+				name: 'Manzanita',
+				forecastUrl: 'http://www.wunderground.com/US/OR/Manzanita.html',
+				iconUrl: 'http://icons-ak.wxug.com/i/c/k/rain.gif',
+				weather: 'Light rain',
+				temp: '55.0 F (12.8 C)'
+			}
+		]
+	};
+}
